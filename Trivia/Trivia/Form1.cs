c1db637e9f2a2bab94b71dfdde1d4a85b79ccf63
DariusBolos <63288528+DariusBolos@users.noreply.github.com>
2020-05-12 11:17:59 +0300

    Add files via upload

--- Trivia/Trivia/Form1.cs
+++ Trivia/Trivia/Form1.cs
@@ -74,6 +74,9 @@ namespace Trivia
             answers.Add("Freddie Mercury");
             answers.Add("John Bon Jovi");
             questions.Add(InitQuestion("Who was the singer of Queen?", answers));
+            answers.Add("Spongebob");
+            answers.Add("Plankton");
+            questions.Add(InitQuestion2("Who was the villain of the show?", answers));
         }
 
         private void ImageClick(object sender, EventArgs e)
@@ -83,6 +86,69 @@ namespace Trivia
             if (pictureBox.Name == questions[0].correctAnswer)
                 MessageBox.Show("Bine");
             else MessageBox.Show("Nu esti roacer adevarat, bro");
+
+            this.Controls.Clear();
+        }
+
+        struct Question2
+        {
+            public string questionString;
+            public List<PictureBox> answers;
+            public string correctAnswer;
+        }
+
+
+        
+        List<Question> questions2 = new List<Question>();
+
+        private Question InitQuestion2(string questionstring, List<string> answers)
+        {
+            Question question = new Question();
+            question.answers = new List<PictureBox>();
+
+            question.questionString = questionstring;
+
+            for (int iterator = 0; iterator < answers.Count; iterator++)
+            {
+                PictureBox auxPictureBox = new PictureBox();
+                if (answers[iterator].Contains("Spongebob"))
+                    auxPictureBox.Image = Properties.Resources.p4;
+                else if (answers[iterator].Contains("Squidward"))
+                    auxPictureBox.Image = Properties.Resources.p5;
+
+                auxPictureBox.Width = 400;
+                auxPictureBox.Height = 400;
+                auxPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                auxPictureBox.Location = new Point(iterator * 500, 200);
+                auxPictureBox.Name = answers[iterator];
+
+                auxPictureBox.Click += new EventHandler(Image2Click);
+                question.answers.Add(auxPictureBox);
+
+                this.Controls.Add(question.answers[iterator]);
+
+            }
+
+            Label questionLabel = new Label();
+            questionLabel.Text = question.questionString;
+            questionLabel.Location = new Point(10, 10);
+            questionLabel.AutoSize = true;
+            questionLabel.Font = new Font("Times New Roman", 24);
+            this.Controls.Add(questionLabel);
+            question.correctAnswer = "Plankton";
+
+            return question;
+
+        }
+
+        private void Image2Click(object sender, EventArgs e)
+        {
+            var pictureBox = (PictureBox)sender;
+
+            if (pictureBox.Name == questions[1].correctAnswer)
+                MessageBox.Show("Bine");
+            else MessageBox.Show("Nu ai copilarie, bro");
         }
     }
 }
+
